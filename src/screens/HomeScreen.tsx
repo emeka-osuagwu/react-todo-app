97584bcc03edcc2594ca6c177cf85e541dc84349
Emeka Osuagwu <emekaosuagwuandela@gmail.com>
2021-03-27 13:19:32 +0100

    inital commit with new dependency

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Todo from "../components/Todo"
+
+function App() {
+    return (
+        <>
+            <h1>Docler FE Homework</h1>
+            <Todo></Todo>
+        </>
+    );
+}
+
+export default App;
